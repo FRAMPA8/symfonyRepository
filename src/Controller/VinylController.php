479119dadff3094d1088660c30f04541170eac33
FRAMPA8 <FNemec@seznam.cz>
2024-02-28 15:32:35 +0100

    twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = array(['artist' => '1st', 'song' => 'song'],
+            ['artist' => '2nd', 'song' => 'song2'],
+            ['artist' => '3rd', 'song' => 'song3']);
+
+        return $this->render('vinyl/homepage.html.twig', ['title' => 'PB & Jams', 'tracks' => $tracks]);
     }
 
     #[Route('/browse/{genre}')]
